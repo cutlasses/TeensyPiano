5153524e0efd02499dd0af1dc6990e85fe2ce65d
Cutlasses <spitkethly@gmail.com>
2018-08-15 18:42:58 +0100

    Changes to allow use on audio board OR RadioMusic

--- TeensyPiano.ino
+++ TeensyPiano.ino
@@ -1,7 +1,8 @@
 #include "SamplePlayer.h"
 #include "AudioSamplePiano_c3_44k.h"
 
-//#define AUDIO_BOARD
+#define AUDIO_BOARD
+#define SHOW_PERF
 
 constexpr int         NUM_VOICES(4);
 
@@ -31,7 +32,6 @@ AudioConnection       patch_cord_3( sample_player_3, 0, sample_mixer, 2 );
 AudioConnection       patch_cord_4( sample_player_4, 0, sample_mixer, 3 );
 AudioConnection       patch_cord_5( sample_mixer, 0, audio_output, 0 );
 
-
 POLYPHONIC_SAMPLE_PLAYER<NUM_VOICES>  polyphonic_sample_player( reinterpret_cast<const uint16_t*>(&(AudioSamplePiano_c3_44k[0])) );
 
 volatile boolean g_triggered = false;
@@ -47,6 +47,13 @@ void setup()
 
   AudioMemory(64);
 
+#ifdef AUDIO_BOARD
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.5f);
+
+  sgtl5000_1.lineOutLevel( 8 );  // 3.16volts p-p
+#else // !AUDIO_BOARD
+  // RADIO MUSIC setup
   analogReference(DEFAULT);
   analogReadRes(ADC_BITS);
   pinMode( NOTE_CV_PIN, INPUT );
@@ -54,13 +61,7 @@ void setup()
 
   // Add an interrupt on the RESET_CV pin to catch rising edges
   attachInterrupt( digitalPinToInterrupt(TRIG_CV_PIN), notify_trigger, RISING );
-
-#ifdef AUDIO_BOARD
-  sgtl5000_1.enable();
-  sgtl5000_1.volume(0.5f);
-
-  sgtl5000_1.lineOutLevel( 8 );  // 3.16volts p-p
-#endif // AUDIO_BOARD
+#endif // !AUDIO_BOARD
   
   polyphonic_sample_player.add_sample_player( sample_player_1 );
   polyphonic_sample_player.add_sample_player( sample_player_2 );
@@ -74,6 +75,7 @@ void setup()
 
 void loop()
 {
+#ifndef AUDIO_BOARD
   if( g_triggered )
   {
     Serial.println("Trigger");
@@ -92,21 +94,42 @@ void loop()
 
     polyphonic_sample_player.play_at_pitch( semitone );
   }
+#endif // !AUDIO_BOARD
 
-  /*
-  const float unary = random(100) / 100.0f;
-  //const float speed = lerp( 0.5, 2.0f, unary );
-  const float speed = 0.5f + ( 1.5f * unary );
-  polyphonic_sample_player.play( speed );
-
-  const int processor_usage = AudioProcessorUsage();
-  if( processor_usage > 50 )
+#ifdef SHOW_PERF
+  int perf_time = millis();
+  static int time_stamp = 0;
+  static int total_processor_usage = 0;
+  static int num_samples = 0;
+  if( perf_time > time_stamp )
   {
-    Serial.print( "Performance spike: " );
-    Serial.print( processor_usage );
-    Serial.print( "\n" );
-  }
+    time_stamp = perf_time + 1000.0f;
+    
+    Serial.print( "Average processor usage:" );
+    Serial.println( static_cast<float>(total_processor_usage) / num_samples );
 
-  delay( random(1000) );
-  */
+    total_processor_usage = 0;
+    num_samples = 0;
+  }
+  else
+  {
+    total_processor_usage += AudioProcessorUsage();
+    ++num_samples;
+  }
+#endif // SHOW_PERF
+
+#ifdef AUDIO_BOARD
+  int time = millis();
+  static int next_play_time = 0;
+  if( time > next_play_time )
+  {
+    const float unary = random(100) / 100.0f;
+    const float speed = 0.5f + ( 1.5f * unary );
+    polyphonic_sample_player.play( speed );
+
+    Serial.print("Play at ");
+    Serial.println( speed );
+    next_play_time = time + random( 1000 );
+  }
+  #endif // AUDIO_BOARD
 }
